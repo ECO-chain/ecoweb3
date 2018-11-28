8c2708a8bbb24593f25fa6a2e6f07f255806111a
Bilal Waebuesa <lord.eagle.b@gmail.com>
2018-11-28 16:51:36 +0700

    Exporting Ecocjs lib

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const Ecocw3 = require('./ecoweb3');
 const Encoder = require('./formatters/encoder');
 const Decoder = require('./formatters/decoder');
 const Utils = require('./utils');
+const Ecocjs = require('./ecocjs');
 
 module.exports = {
   Ecocw3,
   Encoder,
   Decoder,
   Utils,
+  Ecocjs,
 };
